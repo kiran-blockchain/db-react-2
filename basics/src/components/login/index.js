1e505919e22d70d82687892390f1fef0158b03d5
crypto <crypto@cryptos-MBP.Dlink>
2021-08-27 10:59:59 +0530

    Added code

--- basics/src/components/login/index.js
+++ basics/src/components/login/index.js
@@ -1,61 +1,51 @@
 import { useState } from "react";
+import { Formik, Field, Form, ErrorMessage, yupToFormErrors } from "formik";
+import * as Yup from 'yup';
 
 const Login = () => {
-    const [loginDetails, setLoginDetails] = useState({ email: "",isValidEmail:true });
-    const handleChange = (e) => {
-        setLoginDetails({ ...loginDetails, [e.target.name]: e.target.value });
+    const init = {
+        email: ''
     };
-    const handleSubmit = (e) => {
-        validate(e);
-    };
-    const validate = (e) => {
-        if (loginDetails.email && loginDetails.email != '') {
-            loginDetails.isValidEmail = true;
-            loginDetails.emailErrorMessage = '';
-        }
-        else {
-            loginDetails.isValidEmail = false;
-            loginDetails.emailErrorMessage = 'Enter Valid Email';
-            loginDetails.isValidForm = false;
-        }
-        if(!loginDetails.isValidForm){
-            e.preventDefault();
-        }
-        setLoginDetails({ ...loginDetails })
-    }
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().email('Enter Valid Email').required('Email is required')
+    });
     return (
         <div>
             <h1>Login</h1>
-            <form>
-                <div class="container-fluid mt-3">
-                    <div class="row offset-md-3">
-                        <div class="card col-md-8">
-                            <div class={loginDetails.isValidEmail?"row mb-3 mt-3":"row mb-3 mt-3 has-danger"}>
-                                <label for="email" class="col-sm-2 col-form-label">Email</label>
-                                <div class="col-sm-6">
-                                    <input type="text"
-                                       class= {loginDetails.isValidEmail?"form-control":"form-control is-invalid"}
-                                        id="email"
-                                        name="email"
-                                        onChange={handleChange}
-
-                                    />
-                                    <div class="text-danger">{loginDetails.emailErrorMessage}</div>
+            <Formik
+                initialValues={init}
+                validationSchema={validationSchema}
+            >
+                {({ handleSubmit, handleChange, values, errors }) => (
+                    <form onSubmit={handleSubmit}>
+                        <div class="container-fluid mt-3">
+                            <div class="row offset-md-3">
+                                <div class="card col-md-8">
+                                    <div class= "row mb-3 mt-3">
+                                        <label for="email" class="col-sm-2 col-form-label">Email</label>
+                                        <div class="col-sm-6">
+                                            <input
+                                                className="form-control"
+                                                type="text"
+                                                onChange={handleChange}
+                                                name="email" />
+                                                 <div className="text-danger"><ErrorMessage  name="email"/></div>
+                                        </div>
+                                      
+                                    </div>
+                                    <div class="row ">
+                                        <div class="col-md-1 offset-md-4 mt-2 mb-2">
+                                            <button type="submit" class="btn btn-primary">Login</button>
+                                        </div>
+                                    </div>
                                 </div>
 
-
                             </div>
-                            <div class="row ">
-                            <div class="col-md-1 offset-md-4 mt-2 mb-2">
-                                <button type="button" class="btn btn-primary " onClick={handleSubmit}>Login</button>
-                            </div>
-                        </div>
-                        </div>
-                       
-                    </div>
 
-                </div>
-            </form>
+                        </div>
+                    </form>
+                )}
+            </Formik>
         </div >
     )
 }
